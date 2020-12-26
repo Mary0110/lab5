a3bbcdeaa4f2a726d105bc96bf0b379ee13f679a
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2020-12-26 20:24:50 +0300

    Add files via upload

---
+++ 1/My_lib(static)/header.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+double calc_Y(int i);
+
+
+
+
+#endif // HEADER_H_INCLUDED
---
+++ 1/My_lib(static)/lib_main.cpp
@@ -0,0 +1,9 @@
+#include <math.h>
+#include "header.h"
+
+double calc_Y(int i)
+{
+    double y = 0.1 * tan(0.1 * i);
+    return y;
+}
+
